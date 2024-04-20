39c25a2d0a4b9fbda7482b473c997f4c7943b169
Yiseul Park <omjl5123@catholic.ac.kr>
2024-04-20 11:13:01 +0900

    refactor: 파일 엔티티에 확장자 필드 추가

--- src/main/java/com/noticemanagement/notice/domain/File.java
+++ src/main/java/com/noticemanagement/notice/domain/File.java
@@ -26,13 +26,21 @@ public class File {
 	@Column(name = "notice_id", nullable = false)
 	private Long noticeId;
 
-	public File(final Long noticeId) {
+	@Column(name = "extension", nullable = false)
+	private String extension;
+
+	public File(final Long noticeId, final String extension) {
 		this.noticeId = noticeId;
+		this.extension = extension;
 	}
 
 	public static List<File> of(final List<MultipartFile> files, final Long noticeId) {
 		return files.stream()
-			.map(file -> new File(noticeId))
+			.map(file -> {
+				final String originalFilename = file.getOriginalFilename();
+				final String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+				return new File(noticeId, extension);
+			})
 			.toList();
 	}
 }
